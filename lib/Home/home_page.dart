6003df1082eb8062320660792f303f3498b20d28
unknown <henriquedepaulasp@gmail.com>
2019-09-03 08:57:47 -0300

    Versao 2

--- lib/Home/home_page.dart
+++ lib/Home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +9,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    FlutterStatusbarcolor.setStatusBarColor(Colors.transparent);
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Text('Bem Vindo de Volta!'),
+          centerTitle: true,
+        ),
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('')),
+          BottomNavigationBarItem(icon: Icon(Icons.date_range), title: Text('')),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), title: Text('')),
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('')),
+          BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('')),
+        ]),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: 230,
+              decoration: BoxDecoration(
+                  color: Color(0xff5e4ca0),
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50))),
+            ),
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:agenda_smn/login/login_page.dart';
 import 'package:flutter/material.dart';
 
+import 'Home/home_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Login(),
-      theme: ThemeData(primaryColor: Color(0xff1A2649)),
+      home: HomePage(),
+      theme: ThemeData(primaryColor: Color(0xff5e4ca0)),
     );
   }
 }
